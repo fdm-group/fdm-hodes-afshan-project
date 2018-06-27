2b58e58b484bf3bdaffccaa9c81a768b93c199ae
FDMGROUP\michael.ward <michael.ward@fdmgroup.com>
2018-06-27 12:10:28 +0100

    update mailchimp for wp plugin

--- www/wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ www/wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInite220b9cdf73db6b31110859155e3018a::getLoader();
+return ComposerAutoloaderInita077e77aed35665d3812cf40b026e93e::getLoader();
